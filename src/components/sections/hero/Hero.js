1deedf421e5eacbbe99d824c182f83b2c3fa430f
RodrigoNeoEx <draja.acc@gmail.com>
2021-09-05 13:51:42 -0300

    feat: main hero

--- src/components/sections/hero/Hero.js
+++ src/components/sections/hero/Hero.js
@@ -4,25 +4,33 @@ import HeroFooter from "../../footer/HeroFooter"
 import PlayBtn from "../../buttons/PlayBtn"
 import { messages } from "./messages";
 import imageHeader from "../../../images/ImageHeader.png"
+// import heroBackground from "../../../images/heroBackground.png"
+
 import "./style/heroSection.css"
 
 const Hero = () => {
   return (
     <React.Fragment>
-      <Header/>
-      <main className="heroSection">
+      <section className="heroSection">
+        <img className="heroSection__bg"/>
+        <main className="heroSection__main">
 
-        <article className="heroSection__article">
-          <h1>{messages.head}</h1>
-          <p>{messages.description}</p>
-          <PlayBtn/>
-        </article>
+          <Header/>
 
-        <aside className="heroSection__aside">
-          <img src={imageHeader} alt="App Example" className="heroSection__aside__img"/>
-        </aside>
+          <article className="heroSection__article">
+            <h1>{messages.head}</h1>
+            <p>{messages.description}</p>
+            <PlayBtn/>
+          </article>
 
-      </main>
+          <aside className="heroSection__aside">
+            <img src={imageHeader} alt="App Example" className="heroSection__aside__img"/>
+          </aside>
+
+        </main>
+
+      </section>
+
       <HeroFooter/>
     </React.Fragment>
 
