7320c1a3dae2d67c23b559f723bd3eb2aff22c77
RodrigoNeoEx <draja.acc@gmail.com>
2021-09-07 17:46:28 -0300

    feat: component navFooter

--- src/components/footer/NavFooter.js
+++ src/components/footer/NavFooter.js
@@ -4,20 +4,22 @@ import hapu from "../../svg/Logohapu.svg"
 import facebook from "../../images/Facebook.png"
 import twiter from "../../images/Twitter.png"
 import insta from "../../images/Instagram.png"
+import { messages } from "./messages";
 
 const NavFooter = () => {
   return (
-    <footer>
-      <img src={hapu}/>
-      <nav>
-        <a></a>
-        <a></a>
-        <a></a>
+    <footer className="navFooter">
+      <img src={hapu} alt="logo hapu" className="navFooter__logo"/>
+      <nav className="navFooter__links">
+        <a>{messages.shareNany}</a>
+        <a>{messages.ourStory}</a>
+        <a>{messages.blog}</a>
+        <a>{messages.terms}</a>
       </nav>
-      <nav>
-        <a><img src={facebook}/></a>
-        <a><img src={twiter}/></a>
-        <a><img src={insta}/></a>
+      <nav className="navFooter__midias">
+        <a><img src={facebook} alt="logo facebook"/></a>
+        <a><img src={twiter} alt="logo twiter"/></a>
+        <a><img src={insta} alt="logo instagram"/></a>
       </nav>
     </footer>
   )
