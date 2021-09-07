7cd67db2c27f1b61d25c17437a9a560181fd5e59
RodrigoNeoEx <draja.acc@gmail.com>
2021-09-07 14:24:35 -0300

    feat: component frameworkDescription

--- src/components/sections/about/frameworkDescription/FrameworkDescription.js
+++ src/components/sections/about/frameworkDescription/FrameworkDescription.js
@@ -1,16 +1,17 @@
 import React from "react";
 import FrameworkPic from "../../../../images/billingHistory.png"
 import { messages } from "../messages";
+import "./style/framework.css";
 
 const FrameworkDescription = () => {
   return (
-    <section>
-      <article>
-        <h1>{messages.frameworkTitle}</h1>
-        <p>{messages.frameworkDescription}</p>
-        <a>{messages.frameworkLink}</a>
+    <section className="frameworkInfo">
+      <article className="frameworkInfo__article">
+        <h2 className="article__title">{messages.frameworkTitle}</h2>
+        <p className="article__description">{messages.frameworkDescription}</p>
+        <a className="article__link">{messages.frameworkLink}</a>
       </article>
-      <img src={FrameworkPic} alt="app example"/>
+      <img src={FrameworkPic} alt="app example" className="frameworkInfo__pic"/>
     </section>
   )
 }
