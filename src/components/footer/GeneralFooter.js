13af4b9909a996ce385cc77f69f275181593db96
RodrigoNeoEx <draja.acc@gmail.com>
2021-09-07 17:49:08 -0300

    feat: component generalFooter

--- src/components/footer/GeneralFooter.js
+++ src/components/footer/GeneralFooter.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { messages } from "./messages";
 import "./style/footers.css"
 
 const GeneralFooter = () => {
   return (
-    <footer>
-      <p></p>
+    <footer className="generalFooter">
+      <p className="generalFooter__text">{messages.copyright}</p>
     </footer>
   )
 }
