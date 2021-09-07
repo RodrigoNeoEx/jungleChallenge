7d48e8f436d0bc78926baa9127ebdf4121ac6ec7
RodrigoNeoEx <draja.acc@gmail.com>
2021-09-07 14:33:50 -0300

    feat: component comingSoon

--- src/components/sections/about/comingSoon/CommingSoon.js
+++ src/components/sections/about/comingSoon/CommingSoon.js
@@ -1,13 +1,19 @@
 import React from "react";
 import upCommingFunctions from "../../../../images/commingSoon.png"
 import { messages } from "../messages";
+import "./style/commingSoon.css"
 
 const CommingSoon = () => {
   return (
-    <section>
-      <img src={upCommingFunctions} alt="small cartoons with a book tablet and child pic"/>
-      <h1>{messages.commingSoonTitle}</h1>
-      <p>{messages.commingSoonDescription}</p>
+    <section className="commingSoon">
+      <img
+       src={upCommingFunctions}
+       alt="small cartoons with a book tablet and child pic"
+       className="commingSoon__pic"/>
+      <article className="commingSoon__article">
+        <h2 className="article__title">{messages.commingSoonTitle}</h2>
+        <p className="article__description">{messages.commingSoonDescription}</p>
+      </article>
     </section>
   )
 }
