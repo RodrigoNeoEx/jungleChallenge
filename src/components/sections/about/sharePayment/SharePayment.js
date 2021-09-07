2569734cd52cc6d54f263bfd39c071361e3bf24e
RodrigoNeoEx <draja.acc@gmail.com>
2021-09-07 14:12:43 -0300

    feat: component sharePayment

--- src/components/sections/about/sharePayment/SharePayment.js
+++ src/components/sections/about/sharePayment/SharePayment.js
@@ -1,14 +1,17 @@
 import React from "react";
 import PaymentExemple from "../../../../images/sharePayment.png"
 import { messages } from "../messages";
+import "./style/sharedPayment.css"
 
 const SharePayment = () => {
   return (
-    <section>
-      <img src={PaymentExemple} alt="share payment table" />
-      <h1>{messages.sharePaymentTitle}</h1>
-      <p>{messages.sharePaymentDescription}</p>
-      <a>{messages.sharePaymentLink}</a>
+    <section className="sharedPayment">
+      <img src={PaymentExemple} alt="share payment table" className="sharedPayment__img" />
+      <article className="sharedPayment__article">
+        <h2 className="article__title">{messages.sharePaymentTitle}</h2>
+        <p className="article__description">{messages.sharePaymentDescription}</p>
+        <a className="article__link">{messages.sharePaymentLink}</a>
+      </article>
     </section>
   )
 }
