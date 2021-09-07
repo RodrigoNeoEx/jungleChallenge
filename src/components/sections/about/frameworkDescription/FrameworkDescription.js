91a0a7a51ee947efc7e6cd1cce205730c817bda9
RodrigoNeoEx <draja.acc@gmail.com>
2021-09-07 11:18:56 -0300

    feat: component frameworkDescription mvp

--- src/components/sections/about/frameworkDescription/FrameworkDescription.js
+++ src/components/sections/about/frameworkDescription/FrameworkDescription.js
@@ -1,11 +1,17 @@
 import React from "react";
+import FrameworkPic from "../../../../images/billingHistory.png"
 import { messages } from "../messages";
 
 const FrameworkDescription = () => {
   return (
-    <div>
-      hey!
-    </div>
+    <section>
+      <article>
+        <h1>{messages.frameworkTitle}</h1>
+        <p>{messages.frameworkDescription}</p>
+        <a>{messages.frameworkLink}</a>
+      </article>
+      <img src={FrameworkPic} alt="app example"/>
+    </section>
   )
 }
 
