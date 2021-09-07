d8e5a026263a00b2fe660c4111f3b24083acc482
RodrigoNeoEx <draja.acc@gmail.com>
2021-09-07 11:23:19 -0300

    feat: component about mvp

--- src/components/sections/about/comingSoon/CommingSoon.js
+++ src/components/sections/about/comingSoon/CommingSoon.js
@@ -1,11 +1,14 @@
 import React from "react";
+import upCommingFunctions from "../../../../images/commingSoon.png"
 import { messages } from "../messages";
 
 const CommingSoon = () => {
   return (
-    <div>
-      hey!
-    </div>
+    <section>
+      <img src={upCommingFunctions} alt="small cartoons with a book tablet and child pic"/>
+      <h1>{messages.commingSoonTitle}</h1>
+      <p>{messages.commingSoonDescription}</p>
+    </section>
   )
 }
 
